442fd7d8c42a733892ad7c5895bb158f08550d7b
RYV8 <vossanouromaric@gmail.com>
2026-06-20 19:34:21 +0100

    Fix map API root route

--- backend/src/routes/map.js
+++ backend/src/routes/map.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const ctrl = require('../controllers/mapController')
 
+router.get('/', ctrl.getAvailability)
 router.get('/availability', ctrl.getAvailability)
 
 module.exports = router
